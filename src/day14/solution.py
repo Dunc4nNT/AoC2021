fc6770b7884794a9a087c25bc70593b7ce107a69
Dunc4n <76918336+Dunc4nNT@users.noreply.github.com>
2021-12-22 09:58:21 +0100

    add code that doesn't take 2 years to execute :)

--- src/day14/solution.py
+++ src/day14/solution.py
@@ -4,34 +4,58 @@ class ExtendedPolymerization():
     def __init__(self, file):
         with open(file, "r") as f:
             lines = f.read()
-        self.data = list(lines.split("\n\n")[0])
-        self.pairs = {}
-        pairs_data = lines.split("\n\n")[1].split("\n")
-        for pair in pairs_data:
-            pair_lst = pair.split(" -> ")
-            self.pairs.update({pair_lst[0]: pair_lst[1]})
 
-    def increase_steps(self, n):
-        for _ in range(n):
-            count = 0
-            for i in range(len(self.data) - 1):
-                if self.pairs[self.data[count] + self.data[count+1]]:
-                    self.data.insert(count+1, self.pairs[self.data[count] + self.data[count+1]])
-                    count += 2
+        # create a counter with all the start value pairs
+        # this way i can simply update a counter number, rather than a huge string that gets infinitely big :)
+        pairs_data = list(lines.split("\n\n")[0])
+        pairs = []
+        for i in range(len(pairs_data) - 1):
+            pairs.append(pairs_data[i] + pairs_data[i+1])
+        self.counter = Counter(pairs)
+
+        # create a dict with all the "rules" about pairs in them
+        # also create a base dictionary where all rules have a count of 0
+        # used in every step of get_result
+        rules_data = lines.split("\n\n")[1].split("\n")
+        self.rules = {}
+        for rule in rules_data:
+            self.rules.update({rule[0] + rule[1]: [rule[0] + rule[-1], rule[-1] + rule[1]]})
+        self.rules_base_counter = {rule : 0 for rule in self.rules}
+
+    def get_result(self, steps):
+        for _ in range(steps):
+            current_counter = self.rules_base_counter.copy()
+            for k, v in self.counter.items():
+                # increases the values of the new pairs that get created as a result of the old pairs
+                # much, much more efficient than going through every single pair on by one :)
+                current_counter[self.rules[k][0]] += v
+                current_counter[self.rules[k][1]] += v
+            self.counter = current_counter
+
+        # count the amount every letter exists in the final "string"
+        totals = {}
+        for l1, l2 in list(self.counter):
+            totals.update({l1 : 0})
+            totals.update({l2 : 0})
+        for k, v in self.counter.items():
+            totals[k[0]] += v
+        totals[list(self.counter.keys())[-1][1]] += 1   # because the very last value of the "string" is otherwise missed out
+
+        # delete all keys that have a value of 0
+        # this way the Counter returning most common doesn't have a bunch of 0 values in the end
+        # which would make it hard to find the least common letter
+        keys_to_delete = [key for key, value in totals.items() if value == 0]
+        for key in keys_to_delete:
+            del totals[key]
+
+        count = Counter(totals).most_common()
+        least_common = count[-1]
+        most_common = count[0]
+        diff = most_common[1] - least_common[1]
+
+        return diff
+
 
 if __name__ == "__main__":
-    pt1 = ExtendedPolymerization("input.txt")
-    pt1.increase_steps(10)
-    counts = Counter(pt1.data).most_common()
-    least_common = counts[-1]
-    most_common = counts[0]
-
-    print(f"Part 1: {most_common[1] - least_common[1]}")
-
-    pt2 = ExtendedPolymerization("input.txt")
-    pt2.increase_steps(40)
-    counts = Counter(pt2.data).most_common()
-    least_common = counts[-1]
-    most_common = counts[0]
-
-    print(f"Part 2: {most_common[1] - least_common[0]}")
+    print(f"Part 1: {ExtendedPolymerization('input.txt').get_result(10)}")
+    print(f"Part 2: {ExtendedPolymerization('input.txt').get_result(40)}")
